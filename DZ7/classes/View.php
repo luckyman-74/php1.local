712e52716ccd9fc6e5674199162d6f0189291f57
Eugene Solonenko <8661319@mail.ru>
2017-09-07 12:26:55 +0500

    Добавил .gitignore

--- DZ7/classes/View.php
+++ DZ7/classes/View.php
@@ -13,20 +13,17 @@ class View
     public function render(string $template): string
     {
         ob_start(); //Включаем буферизацию вывода
-        extract($this->data, EXTR_SKIP); //Импортируем переменные из массива. Если переменная с таким именем существует, ее текущее значение не будет перезаписано.
-
-        require $template; //Подгружаем наш шаблон
-
+        foreach ($this->data as $key => $value) {
+            $$key = $value;
+        }
+        require $template;
         return ob_get_clean(); //Получаем содержимое текущего буфера и затем удаляем текущий буфер
     }
 
 
     public function display(string $template): void
     {
-        foreach ($this->data as $key => $value) {
-            $$key = $value;
-        }
-        require $template;
+        echo $this->render($template);
     }
 
 }
