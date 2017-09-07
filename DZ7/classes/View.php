50efe8566bd94b1782e63b613e7487aa4f45937d
Eugene Solonenko <8661319@mail.ru>
2017-09-07 12:43:36 +0500

    Добавил .gitignore

--- DZ7/classes/View.php
+++ DZ7/classes/View.php
@@ -13,18 +13,18 @@ class View
     public function render(string $template): string
     {
         ob_start(); //Включаем буферизацию вывода
-        foreach ($this->data as $key => $value) {
+        foreach ($this->data as $key => $value) { //Извлекаем переменные  из массива
             $$key = $value;
         }
         /* @noinspection PhpIncludeInspection */
-        require $template;
+        require $template; //Подключаем шаблон. В нем будут доступны переданные данные в виде переменных
         return ob_get_clean(); //Получаем содержимое текущего буфера и затем удаляем текущий буфер
     }
 
 
     public function display(string $template): void
     {
-        echo $this->render($template);
+        echo $this->render($template); //Пропускаем шаблон через метод render и выводим его в поток
     }
 
 }
